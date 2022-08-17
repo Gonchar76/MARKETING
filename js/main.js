dcb204bd9a1632d9b8866203f6997817ae98eaec
Gonchar76 <gonchar76@internet.ru>
2022-08-17 19:16:12 +0300

    адаптировал портфолио

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(function () {
-  var mixer = mixitup(".porfolio__content");
-
   $(".slider-blog__inner").slick({
     dots: true,
     prevArrow:
@@ -8,4 +6,10 @@ $(function () {
     nextArrow:
       '<button type="button" class="slick-next"> <img src="img/arrow-left.svg" alt="arrow-left"></button>',
   });
+
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleClass("menu__list_active");
+    $(".header__top-inner").toggleClass("header__top-inner_active");
+  });
+var mixer = mixitup(".porfolio__content");
 });
