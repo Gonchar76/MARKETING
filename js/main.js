8ff3a4d092dd831e1436b19194a03b2525b83102
Gonchar76 <gonchar76@internet.ru>
2022-08-18 18:09:46 +0300

    сделал сайт под мобилы

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 $(function () {
+$(".menu a,.go-top").on("click", function (e) {
+  e.preventDefault();
+  var id = $(this).attr("href"),
+    top = $(id).offset().top;
+  $("body,html").animate({ scrollTop: top }, 1500);
+});
+
   $(".slider-blog__inner").slick({
     dots: true,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="img/arrow-right.svg" alt="arrow-right"></button>',
     nextArrow:
       '<button type="button" class="slick-next"> <img src="img/arrow-left.svg" alt="arrow-left"></button>',
-  });
+        responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+    arrows:false
+      }
+    },
+  ]
+});
 
-  $(".menu__btn").on("click", function () {
+  $(".menu__btn, .menu__list-item a").on("click", function () {
     $(".menu__list").toggleClass("menu__list_active");
     $(".header__top-inner").toggleClass("header__top-inner_active");
   });
+  
 var mixer = mixitup(".porfolio__content");
 });
